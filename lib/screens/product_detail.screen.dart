4615ce223ae5ffb0ebb4dad43f94f0203bbf7d9f
Darshan Satya <darshans1989@gmail.com>
2019-12-20 21:52:17 -0500

    Get the data in product details screen through provider

--- lib/screens/product_detail.screen.dart
+++ lib/screens/product_detail.screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product_detail';
@@ -6,10 +8,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(
+        productId); // false because I do not want this to rebuild when the product is added because it wont make a difference
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
     );
   }
