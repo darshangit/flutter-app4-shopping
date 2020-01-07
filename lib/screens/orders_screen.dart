11f1def9089e4ef333ab53097c4665d77f266e3f
Darshan Satya <darshans1989@gmail.com>
2020-01-06 21:34:28 -0500

    Making orderscreen stateless

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,28 +4,8 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    // super.initState();
-    // setState(() {
-    //   _isLoading = true;
-    // });
-    // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-    //   setState(() {
-    //     _isLoading = false;
-    //   });
-    // });
-  }
 
   @override
   Widget build(BuildContext context) {
